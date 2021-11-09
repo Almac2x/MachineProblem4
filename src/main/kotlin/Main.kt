4ce1f66a5b8d1c262afc43e86f5f81a851a681dd
bland <blando_macmac@yahoo.com>
2021-11-09 17:22:26 +0800

    [Just a commit]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.io.InputStream
 import java.security.MessageDigest
 
 fun main(args: Array<String>) {
@@ -23,6 +24,7 @@ fun readInput() : HashMap<String,String>{
             }
 
             "c"->{
+                check()
                 println("End")
             }
             "q" ->{
@@ -36,6 +38,64 @@ fun readInput() : HashMap<String,String>{
 
     return HashMap<String,String>()
 }
+
+fun check() {
+
+    val directory = File("files//") // gets
+    val numberOfBlocks = directory.list().size //size of the directory
+    val hashMapOfBlocks = mutableMapOf<String, String>() // hash of blocks inside the txt blocks
+    var listFiles = directory.listFiles() // list of files in the directory
+
+    var justHashFiles = mutableMapOf<String,String>()
+
+    // Declare our Hasher
+    val shaDigest = MessageDigest.getInstance("SHA-256")
+    val hash256 : Hash256 = Hash256()
+
+
+    for (file in listFiles) {
+        //gets the hashes inside the txt blocks
+        hashMapOfBlocks[file.name] = file.readLines().last() // gets the last line of the txt file which are the hash of the previous block
+        //hashes all the txt blocks again to compare the contents of the txt blocks
+        justHashFiles[file.name] = hash256.getFileChecksum(shaDigest,File("files//${file.name}")).toString()
+    }
+
+    val accountedBlocks = mutableListOf<String>()
+    //check if all blocks are accounted for
+    for(i in 0 until numberOfBlocks){
+        val block = "block${i}"
+        if(hashMapOfBlocks[block] != null){ // checks if the block is in order and name did not change
+            accountedBlocks.add(block)
+        }
+        else
+            break
+    }
+
+
+    // countdown to zero
+    for (i in hashMapOfBlocks.size-1 downTo  0 ){
+
+        if( i == hashMapOfBlocks.size-1){
+            if(hashMapOfBlocks["trailingBlock"] == justHashFiles["block${i-1}"])
+                println(true)
+
+        }else {
+            if(hashMapOfBlocks["block${i}"] == justHashFiles["block${i-1}"]){
+                println(true)
+            }
+            else
+                false
+        }
+    }
+
+    // check if all the blocks are chained together to know if one is tampered
+
+    //gets the hash of each txt block inside
+
+    println()
+} // end of Check
+
+
 
 fun encode() {
 
@@ -73,6 +133,14 @@ fun encode() {
 
     }while(reader != "q")
 
+    //create trailing block for hash of the last transaction block
+    val shaDigest = MessageDigest.getInstance("SHA-256")
+    // Declare our Hasher
+    val hash256 : Hash256 = Hash256()
+
+    File("files//trailingBlock").printWriter().use { out ->
+        out.println(hash256.getFileChecksum(shaDigest,File("files//block${iterator-1}"))) // 4-1 = 3
+    }
 
     println("Encoding End")
 }
@@ -86,32 +154,23 @@ fun createBlockFile(blockNumber : Int, transactions : MutableList<String> ){
     val blockName ="block${blockNumber}" // concatenates blockNumber to blockName
     File("files//${blockName}").printWriter().use { out ->
 
-        for(i in 0 until transactions.size)
+        for (i in 0 until transactions.size)
 
-            if(i == transactions.size-1){
+            if (i == transactions.size - 1) {
 
-                out.println(transactions[i].uppercase()+"\n"+ hash256.getFileChecksum(shaDigest, File("files//block${blockNumber-1}"))) //write the hash of the previous block
-            }else{
+                out.println(
+                    transactions[i].uppercase() + "\n" + hash256.getFileChecksum(
+                        shaDigest,
+                        File("files//block${blockNumber - 1}")
+                    )
+                ) //write the hash of the previous block
+            } else {
                 out.println(transactions[i].uppercase())
             }
 
 //        transactions.forEach {
 //            out.println("${it.uppercase()}")
 //        }
+
     }
 }
-
-
-
-
-// Dito Muna
-//
-////Create checksum for this file
-//val file = File("files//testOut.txt")
-//
-////Use SHA-1 algorithm
-//val shaDigest = MessageDigest.getInstance("SHA-256")
-//
-////SHA-1 checksum
-//val shaChecksum: String? = hash256.getFileChecksum(shaDigest, file)
-//println(shaChecksum)
